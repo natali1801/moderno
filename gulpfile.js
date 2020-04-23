d43b27c6d9f1cef8db3b952c8b046d4eb0d871ec
natali1801-dev <biluhana@icloud.com>
2020-04-23 18:39:14 +0300

    added content, aside, popup, 404, about

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ gulp.task('style', function () {
         'node_modules/slick-carousel/slick/slick.css',
         'node-modules/magnific-popup/dist/magnific-popup.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
-        'node_modules/ion-rangeSlider/css/ion.rangeSlider.css'
+        'node_modules/ion-rangeSlider/css/ion.rangeSlider.css',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css'
     ], { allowEmpty: true })
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -38,7 +39,8 @@ gulp.task('script', function () {
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
-        'node_modules/ion-rangeSlider/js/ion.rangeSlider.js'
+        'node_modules/ion-rangeSlider/js/ion.rangeSlider.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'
     ], { allowEmpty: true })
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
